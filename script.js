2892fff544f560bd5de00cae5d56fb9b64ca7005
Sadulla8887 <115067461+Sadulla8887@users.noreply.github.com>
2023-07-17 11:25:16 +0500

    Sherigacha qildim

--- script.js
+++ script.js
@@ -20,3 +20,23 @@
     }
 
 Проверить, чтобы все работало без ошибок в консоли */
+
+const numberOfFilms = prompt('1 oyni ichida nechta film kurdingiz?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Oxirgi kurgan filmlaringizdan qaysinisini sizga kuproq yoqdi', ''),
+    b = +prompt('Uni nechi baxolagan bular edingiz', ''),
+    c = prompt('Yana brorta uzizga yoqanlaridan tavfsiya eting', ''),
+    d = +prompt('Bunixam bir baxolab bering', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
